d91c71ca43bd2de63975beadff4734c40e3e5d70
Wayne <yky32is@gmail.com>
2022-06-02 18:50:58 +0800

    Fix

--- profile-service/src/main/java/com/quinsic/profileservice/endpoint/Test.java
+++ profile-service/src/main/java/com/quinsic/profileservice/endpoint/Test.java
@@ -2,15 +2,17 @@ package com.quinsic.profileservice.endpoint;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 @Slf4j
 public class Test {
 
-    @GetMapping("/test/as")
-    public String testauth() {
-        return "this is from auth server";
+    @GetMapping("/")
+    public String test() {
+        return "this is from ProfileServiceApplication";
     }
 
 }
--- user-service/src/main/java/com/quinsic/userservice/endpoint/UsersEndpoint.java
+++ user-service/src/main/java/com/quinsic/userservice/endpoint/UsersEndpoint.java
@@ -2,17 +2,18 @@ package com.quinsic.userservice.endpoint;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/users")
 @Slf4j
-public class Test {
+public class UsersEndpoint {
 
-    @GetMapping()
-    public String test() {
-        return "this is from UserServiceApplication";
+    @GetMapping("/{id}")
+    public String getUser(@PathVariable("id") Long id) {
+        log.info("getUser {}", id);
+        return "this is from users.";
     }
-
 }
