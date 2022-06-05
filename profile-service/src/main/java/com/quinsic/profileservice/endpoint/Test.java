541b685e1068623d7a1dbd60c812f24c32362dbe
Wayne Yu <wayneyu.dev@gmail.com>
2022-06-06 03:49:34 +0800

    Cancel Cron Job

--- profile-service/src/main/java/com/quinsic/profileservice/endpoint/Test.java
+++ profile-service/src/main/java/com/quinsic/profileservice/endpoint/Test.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class Test {
 
-    @GetMapping("/")
+    @GetMapping()
     public String test() {
         return "this is from ProfileServiceApplication";
     }
