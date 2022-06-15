2438ba1536ab46b728e490ae00fef8e602b070ba
wayne.yu <wayne.yu@eqonex.com>
2022-06-15 18:30:05 +0800

    Add AuditEntity in app-core

---
+++ app-core/src/main/java/com/quinsic/appcore/entity/AuditEntity.java
@@ -0,0 +1,41 @@
+package com.quinsic.appcore.entity;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.InstantDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.InstantSerializer;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
+import java.io.Serializable;
+import java.time.Instant;
+
+
+@MappedSuperclass
+@Getter
+@Setter
+@EntityListeners(value = AuditingEntityListener.class)
+public class AuditEntity implements Serializable {
+    @Version
+    @Column(name = "version")
+    protected int version;
+
+    @Column(name = "create_dt")
+    @CreatedDate
+    @JsonDeserialize(using = InstantDeserializer.class)
+    @JsonSerialize(using = InstantSerializer.class)
+    protected Instant createDt;
+
+    @Column(name = "update_dt")
+    @LastModifiedDate
+    @JsonDeserialize(using = InstantDeserializer.class)
+    @JsonSerialize(using = InstantSerializer.class)
+    protected Instant updateDt;
+}
